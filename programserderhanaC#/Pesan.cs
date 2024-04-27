666cf27aa6cecca66d44bb8ebf179587a1a8cb14
Adipramana <herryprasetyo.dev@gmail.com>
2024-04-27 16:46:23 +0700

    contoh C#

---
+++ programserderhanaC#/Pesan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programserderhanaC_
+{
+    internal class Pesan
+    {
+        public void TampilkanPesan(string pesan)
+        {
+            Console.WriteLine(pesan);
+        }
+    }
+}
