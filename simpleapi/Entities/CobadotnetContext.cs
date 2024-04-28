ed3ce319768c13519b049d888a180f69d21c32ca
Adipramana <herryprasetyo.dev@gmail.com>
2024-04-28 11:44:42 +0700

    nwe

---
+++ simpleapi/Entities/CobadotnetContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+
+
+namespace simpleapi.Entities;
+
+public partial class CobadotnetContext : DbContext
+{
+    public CobadotnetContext()
+    {
+    }
+
+    public CobadotnetContext(DbContextOptions<CobadotnetContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=;database=cobadotnet");
+            }
+        }
+   
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("product");
+
+            entity.Property(e => e.Harga)
+                .HasMaxLength(32)
+                .HasColumnName("harga");
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Nama)
+                .HasMaxLength(32)
+                .HasColumnName("nama");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ simpleapi/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace simpleapi.Entities;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Nama { get; set; } = null!;
+
+    public string Harga { get; set; } = null!;
+}
